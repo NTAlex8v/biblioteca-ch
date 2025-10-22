09fe4835ac8db9c5ba5cf5ea779043f0412e6fac
Juan Alexander Miranda Quispe <u22234838@gmail.com>
2025-10-22 12:34:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-116270992-844d2",
+  "appId": "1:1054283690209:web:cb150b90140deb6daa07a0",
+  "apiKey": "AIzaSyAsT-UqZJKitPcQUrUnD_eBa8KSLgL2-ww",
+  "authDomain": "studio-116270992-844d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1054283690209"
+};
