abfd8be05f434558f9dd1ed30a2004af40a3a453
Juan Alexander Miranda Quispe <u22234838@gmail.com>
2025-10-24 17:31:30 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/app/document-form.tsx
+++ src/app/document-form.tsx
@@ -1,7 +1,6 @@
-
 "use client";
 
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useRouter, useSearchParams } from "next/navigation";
