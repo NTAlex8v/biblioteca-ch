721e212eb4991ee5b78775dcaf42a1044524bfa6
Juan Alexander Miranda Quispe <u22234838@gmail.com>
2025-10-27 02:49:49 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/document-form.tsx
+++ src/app/document-form.tsx
@@ -10,7 +10,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
 import { useToast } from "@/hooks/use-toast";
 import { useCollection, useFirestore, setDocumentNonBlocking, addDocumentNonBlocking, useMemoFirebase, useUser } from "@/firebase";
 import { collection, doc } from "firebase/firestore";
@@ -328,4 +328,3 @@ export default function DocumentForm({ document }: DocumentFormProps) {
     );
 }
 
-    
