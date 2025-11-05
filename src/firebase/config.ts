92f0747e95ed8067a0536a1d734712d1e25b8b24
Juan Alexander Miranda Quispe <u22234838@gmail.com>
2025-11-05 17:27:51 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
 export const firebaseConfig = {
   "projectId": "studio-116270992-844d2",
   "appId": "1:1054283690209:web:cb150b90140deb6daa07a0",
-  "storageBucket": "studio-116270992-844d2.appspot.com",
   "apiKey": "AIzaSyAsT-UqZJKitPcQUrUnD_eBa8KSLgL2-ww",
   "authDomain": "studio-116270992-844d2.firebaseapp.com",
   "measurementId": "",
--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -7,6 +7,7 @@ import {
   deleteDoc,
   CollectionReference,
   DocumentReference,
+  SetOptions,
 } from 'firebase/firestore';
 import { errorEmitter } from '@/firebase/error-emitter';
 import {FirestorePermissionError} from '@/firebase/errors';
@@ -15,8 +16,8 @@ import {FirestorePermissionError} from '@/firebase/errors';
  * Initiates a setDoc operation for a document reference.
  * Does NOT await the write operation internally.
  */
-export function setDocumentNonBlocking(docRef: DocumentReference, data: any) {
-  setDoc(docRef, data, { merge: true }).catch(error => {
+export function setDocumentNonBlocking(docRef: DocumentReference, data: any, options: SetOptions) {
+  setDoc(docRef, data, options).catch(error => {
     errorEmitter.emit(
       'permission-error',
       new FirestorePermissionError({
