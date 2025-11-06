4dbe56bb88150e7cfe5a8a79c15d3170cabcf1c3
Juan Alexander Miranda Quispe <u22234838@gmail.com>
2025-11-06 07:00:28 +0000

    Try fixing this error: `Runtime ReferenceError: collection is not define

--- src/components/document-list-item.tsx
+++ src/components/document-list-item.tsx
@@ -21,7 +21,7 @@ import {
 import { useToast } from '@/hooks/use-toast';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
-import { doc } from 'firebase/firestore';
+import { doc, collection } from 'firebase/firestore';
 import { useState } from 'react';
 import MoveDocumentDialog from './move-document-dialog';
 
